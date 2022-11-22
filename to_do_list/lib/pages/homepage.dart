a98458670cd29aaa36d1c6e9ebeb6bb77f5ed734
Vinícius Soares <viniciussoares.coding@gmail.com>
2022-11-22 06:32:38 -0300

    Input and ElevatedButton
    Some home UI started to be built. Now it has a input TextField() and a ElevatedButton.

--- to_do_list/lib/pages/homepage.dart
+++ to_do_list/lib/pages/homepage.dart
@@ -5,6 +5,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Add a Task",
+                    hintText: "Ex. Study Flutter",
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Icon(Icons.add, size: 30,),
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color(0xff00d7f3),
+                    padding: const EdgeInsets.all(14),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
